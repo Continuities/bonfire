e77616f39e78b24059e3ba27ab5e4028e57d724b
Michael Townsend <michael@doublespeakgames.com>
2025-09-02 19:06:48 -0400

    more better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <Paper>
   <Title>Did you know?</Title>
   <Content>
-    Certain terms and phrases at the core of the "burner" identity are actually intellectual property, and that to use them we need to license them from The Burning Man Project? Things like "Burning Man," "Decompression Party," and all of the Ten Principles.
-    To license this stuff, we agree to behave within certain cultural norms defined by The Burning Man Project. This agreement does not work both ways, and licensors have no control over the brand that they have licensed or the behaviour of The Burning Man Project itself.
+    Certain terms and phrases at the core of the "burner" identity are actually intellectual property, and that using them requires a license from The Burning Man Project? Things like "<strong>Burning Man</strong>," "<strong>Decompression Party</strong>," and all of the <strong>Ten Principles</strong>.
+    To license this stuff, we agree to behave within certain cultural norms defined by The Burning Man Project. This agreement does not work both ways, and licensors have no control over the brand that they have licensed nor the behaviour of The Burning Man Project itself.
   </Content>
 </Paper>
