4e297f15bf54558ae68b8c15998ba52e07473198
Michael Townsend <michael@doublespeakgames.com>
2025-09-03 14:36:48 -0400

    layout work

---
+++ src/view/Footer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Paper from '@smui/paper';
+	import { _ } from 'svelte-i18n';
+</script>
+
+<footer class="footer mdc-elevation--z24">
+	<p class="license">{@html $_('license')}</p>
+</footer>
+
+<style>
+	.footer {
+		position: fixed;
+		bottom: 0;
+		width: 100vw;
+		background: var(--mdc-theme-surface);
+		padding: 1rem;
+	}
+	.license {
+		font-size: 0.75rem;
+		margin: 0;
+	}
+</style>
