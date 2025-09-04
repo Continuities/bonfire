56f7cc5f4385357790e95474d66fc17d8b734ad0
Michael Townsend <michael@doublespeakgames.com>
2025-09-03 20:39:13 -0400

    deployable to Google App Engine

--- src/view/Footer.svelte
+++ src/view/Footer.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
+	import { mdiGithub } from '@mdi/js';
+	import SvgIcon from './SvgIcon.svelte';
 </script>
 
 <footer class="footer mdc-elevation--z24">
 	<p class="license">{@html $_('license')}</p>
+	<a href="https://github.com/Continuities/openburn" target="_blank" class="github"
+		><SvgIcon path={mdiGithub} /></a
+	>
 </footer>
 
 <style>
@@ -11,10 +16,17 @@
 		bottom: 0;
 		width: 100vw;
 		background: var(--mdc-theme-surface);
-		padding: 1rem;
+		padding: 1rem 2rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 	}
 	.license {
 		font-size: 0.75rem;
 		margin: 0;
+	}
+	.github {
+		display: block;
+		width: 24px;
 	}
 </style>
