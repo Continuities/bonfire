70e2f171e8ba45f6511750875e44f222b36ab832
Michael Townsend <michael@doublespeakgames.com>
2025-09-28 18:02:26 -0400

    shitty emoji picker for valor dialog

---
+++ src/view/EmojiPicker.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Autocomplete from '@smui-extra/autocomplete';
+	import emojis from '$lib/emojis';
+
+	type Props = {
+		value?: String | undefined;
+		label?: string | undefined;
+		required?: boolean | undefined;
+	};
+
+	let { value = $bindable(), label, required }: Props = $props();
+</script>
+
+<Autocomplete
+	bind:value
+	textfield$required={required}
+	options={emojis}
+	showMenuWithNoInput={true}
+	{label}
+/>
