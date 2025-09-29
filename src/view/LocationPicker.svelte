61b313b9138eb0a5abba82c9bb64827134cc2fda
Michael Townsend <michael@doublespeakgames.com>
2025-09-29 14:29:07 -0400

    working on a basic but functional city picker

---
+++ src/view/LocationPicker.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Autocomplete from '@smui-extra/autocomplete';
+	import { Country, State, City, type ICity } from 'country-state-city';
+
+	const MAX_HASH_KEY_LENGTH = 5;
+
+	type Props = {
+		value?: String | undefined;
+		label?: string | undefined;
+		name?: string | undefined;
+		required?: boolean | undefined;
+	};
+
+	let { value = $bindable(), label, required, name }: Props = $props();
+	let text = $state('');
+
+	const getKey = (str: string) => str.substring(0, MAX_HASH_KEY_LENGTH).toLowerCase();
+
+	const allCities: ICity[] = City.getAllCities();
+	const cityHash = allCities.reduce<Record<string, ICity[]>>((hash, city) => {
+		const key = getKey(city.name);
+		if (!hash[key]) {
+			hash[key] = [];
+		}
+		hash[key].push(city);
+		return hash;
+	}, {});
+
+	let currentKey = $derived(getKey(text));
+	let cities = $derived(cityHash[currentKey] ?? []);
+
+	$effect(() => console.log(value));
+
+	const getCityLabel = (city: ICity) => {
+		if (!city) return '';
+		return `${city.name}, ${city.stateCode}, ${city.countryCode}`;
+	};
+</script>
+
+<Autocomplete
+	bind:value
+	bind:text
+	textfield$required={required}
+	textfield$input$name={name}
+	options={cities}
+	showMenuWithNoInput={false}
+	{label}
+	getOptionLabel={getCityLabel}
+/>
