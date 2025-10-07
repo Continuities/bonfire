6113a5e084fc029e3031ec5d2ec970b8f64abcd3
Michael Townsend <michael@doublespeakgames.com>
2025-10-07 14:26:15 -0400

    Added tool filter to Network filter UI

--- src/view/CommunityFilters.svelte
+++ src/view/CommunityFilters.svelte
@@ -3,23 +3,31 @@
 	import LocationPicker from './LocationPicker.svelte';
 	import Stack from './Stack.svelte';
 	import { locationsEqual } from '$lib/location';
+	import ToolPicker from './ToolPicker.svelte';
+	import { getStores } from '$lib/context';
 
 	type Props = {
 		filter: Filter.CommunityFilter;
 		onchange: (filter: Filter.CommunityFilter) => void;
 	};
 	let { filter, onchange }: Props = $props();
+	let { tools } = getStores();
 	let location = $derived(filter.location);
+	let uses_tool = $derived(filter.uses_tool ? $tools[filter.uses_tool] : undefined);
 
 	$effect(() => {
 		if (!locationsEqual(location, filter.location)) {
 			onchange({ ...filter, location });
 		}
+		if (uses_tool?.id !== filter.uses_tool) {
+			onchange({ ...filter, uses_tool: uses_tool?.id });
+		}
 	});
 </script>
 
-<Stack>
+<Stack direction="row">
 	<LocationPicker cities states countries label={$_('location_filter')} bind:value={location} />
+	<ToolPicker label={$_('tool_filter')} bind:value={uses_tool} />
 </Stack>
 
 <style>
